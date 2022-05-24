e3c19017c6d773c8f7886a324f78c2a2b5ba1593
Matan Work <matanmatan999new@gmail.com>
2022-05-24 13:11:54 +0900

    minor fixes to types in cookies utils

--- utils/cookies.ts
+++ utils/cookies.ts
@@ -16,9 +16,9 @@ export const setCookie = (
   const stringValue =
     typeof value === 'object' ? 'j:' + JSON.stringify(value) : String(value);
 
-  if ('maxAge' in options) {
-    options.expires = new Date(Date.now() + options.maxAge);
-    options?.maxAge /= 1000;
+  if (options?.maxAge) {
+    options.expires = new Date(Date.now() + options?.maxAge);
+    options.maxAge /= 1000;
   }
 
   res.setHeader('Set-Cookie', serialize(name, stringValue, options));
